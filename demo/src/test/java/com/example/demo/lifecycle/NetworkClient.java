4148fc37e6491d13a715a1e3dd90294b84e57d65
eugene0318 <nttott@gmail.com>
2024-07-13 07:31:25 +0900

    ~prototype

--- demo/src/test/java/com/example/demo/lifecycle/NetworkClient.java
+++ demo/src/test/java/com/example/demo/lifecycle/NetworkClient.java
@@ -3,6 +3,9 @@ package com.example.demo.lifecycle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 public class NetworkClient{
 	private String url;
 
@@ -28,13 +31,13 @@ public class NetworkClient{
 	public void disconnect() {
 		System.out.println("close:" + url);
 	}
-
+	@PostConstruct
 	public void init() throws Exception {
         System.out.println("NetworkClient.afterPropertiesSet");
         connect();
         call("초기화 연결 메시지");
     }
-
+	@PreDestroy
     public void close() throws Exception {
         System.out.println("NetworkClient.destroy");
         disconnect();
---
+++ demo/src/test/java/com/example/demo/scope/SingletonWithPrototypeTest1.java
@@ -0,0 +1,46 @@
+package com.example.demo.scope;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import lombok.Getter;
+
+public class SingletonWithPrototypeTest1 {
+
+	@Test
+    void prototypeFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        prototypeBean1.addCount();
+        assertThat(prototypeBean1.getCount()).isEqualTo(1);
+
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+        prototypeBean2.addCount();
+        assertThat(prototypeBean1.getCount()).isEqualTo(1);
+    }
+
+    @Getter
+    @Scope("prototype")
+    static class PrototypeBean {
+        private int count = 0;
+
+        public void addCount() {
+            count++;
+        }
+
+        @PostConstruct
+        public void init() {
+            System.out.println("PrototypeBean.init" + this);
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("PrototypeBean.destroy");
+        }
+    }
+}
