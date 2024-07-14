1c9d3e400573d0ef7eb1e87a97ef23fef5bc09b3
eugene0318 <nttott@gmail.com>
2024-07-15 02:37:14 +0900

    ~end

--- demo/src/main/java/com/example/demo/web/LogDemoService.java
+++ demo/src/main/java/com/example/demo/web/LogDemoService.java
@@ -1,13 +1,17 @@
 package com.example.demo.web;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
+
+import com.example.demo.common.MyLogger;
 
 @Service
 @RequiredArgsConstructor
 public class LogDemoService {
-    private final com.example.demo.common.MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
     public void logic(String id) {
+        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.log("service id = " + id);
     }
 }
